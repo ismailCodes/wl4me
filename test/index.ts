d21c370aa4e51b2dcb687f66a6890f6a07a4c476
Ismail EL Moudni <el.ismail@boom-digital.ma>
2022-02-21 22:35:13 +0100

    Added newly deployed contract tests

---
+++ test/index.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Newly deployed contract", function () {
+  it("Should return false for a non existing project", async function () {
+    const Whitelister = await ethers.getContractFactory("Whitelister");
+    const whitelister = await Whitelister.deploy();
+    await whitelister.deployed();
+
+    expect((await whitelister.projects(0)).isWhitelistingActive).to.equal(
+      false
+    );
+  });
+
+  it("Should raise error if trying to add an address to a non existing project", async function () {
+    const Whitelister = await ethers.getContractFactory("Whitelister");
+    const whitelister = await Whitelister.deploy();
+    await whitelister.deployed();
+
+    await expect(whitelister.addAddressToWhitelist(0)).to.be.revertedWith(
+      "Whitelisting not active"
+    );
+  });
+});
